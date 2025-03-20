063dc1c100c4dc1472271b036539d73032044e35
Lucas Goulart Salman <lucas.salman@engdb.com.br>
2025-03-19 21:12:32 -0300

    estrutura do site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PaginaInicialComponent }, // Página inicial
+  // { path: 'home', component: HomeComponent }, // Opcional: acessível por "/home"
+  { path: '**', redirectTo: '' } // Redireciona qualquer rota desconhecida para a home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
