ce5e3af6abad7b5c8f724fd6b5cf9f43576f1bc8
Lucas Goulart Salman <lucas.salman@engdb.com.br>
2025-06-22 04:08:09 -0300

    adptando mobile header

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.menu-open') menuOpen = false;
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+  document.body.classList.toggle('menu-open', this.menuOpen);
+}
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
